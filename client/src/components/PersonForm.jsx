3744f355fd4725786b222026063e2abf3ff05729
Fatuma Meshalla <mompround@gmail.com>
2023-05-10 15:01:13 -0700

    updated one

--- client/src/components/PersonForm.jsx
+++ client/src/components/PersonForm.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import axios from 'axios'
 const PersonForm = () => {
-    const [productList, setProductList] = useState({
+    const [item, setItem] = useState({
         title :'',
         price :'',
         description:''
@@ -10,21 +10,19 @@ const PersonForm = () => {
     const onSubmitHandler = (e)=> {
     e.preventDefault();
         // make a post request to create a new person
-    // useEffect(()=>{
-        axios.post('http://localhost:8000/api/products',{
-        productList
-
-            // [] array of all variable we want to keep track of 
-    }, [])
+    
+        axios.post('http://localhost:8000/api/products',
+        item )
+    
         .then(res =>{
             console.log(res);
             console.log(res.data);
-            setProductList("")
+            setItem("")
         })
         .catch(err => console.log(err))
     }
     const ChangeHandler = (e)=>{
-        setProductList({...productList, [e.target.name]: e.target.value})
+        setItem({...item, [e.target.name]: e.target.value})
     }
   return (
     <div>
@@ -33,15 +31,16 @@ const PersonForm = () => {
                 <h1 className='text-center'> Product Manager</h1>
                 <div className='form-group col-m2'>
                     <label htmlFor="title"> Title</label>
-                    <input type="text" name = "title"  onChange = {ChangeHandler} className='form-control mt-2'  />
+                
+                    <input type="text" name = "title"  onChange = {ChangeHandler} className='form-control mt-2'  value={item.title}  />
                 </div>
                 <div className='form-group'>
                     <label htmlFor="price"> Price</label>
-                    <input type="number" name = "price" onChange={ChangeHandler} className='form-control mt-2'  />
+                    <input type="number" name = "price" onChange={ChangeHandler} className='form-control mt-2'  value={item.price}     />
                 </div>
                 <div className='form-group'>
-                    <label htmlFor="description"> Description</label>
-                    <input type="text" name = "description" onChange={ChangeHandler}  className='form-control mt-2 ' />
+                    <label htmlFor="description"> Description </label>
+                    <input type="text" name = "description" onChange={ChangeHandler}  className='form-control mt-2 '  value={item.description}/>
                 </div>
                 <button class="btn btn-primary" type="submit">Submit</button>
                 <div>
@@ -50,9 +49,6 @@ const PersonForm = () => {
                 </div>
                
             </form>
-
-            
-            
 
         </div>
         
--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,9 +1,11 @@
 const Product = require('../models/product.model')
 module.exports = {
     createProduct: (req,res)=>{
+        console.log( "i am here",req)
+        console.log( "i am here",req.body)
         Product.create(req.body)
         .then(newProduct =>res.json(newProduct))
-        .catch(err => res.status(400).json(`something went wrong ${err}`))
+        .catch(err => {console.log(err); res.status(400).json(`something went wrong ${err}`) })
     },
     getAllProduct : (req,res) =>{
         Product.find()
